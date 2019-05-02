779ad7cfd624b7185dd07670bcc5a847e2cfddf1
Aaron Lipinski <kris.lipinski@gmail.com>
2019-05-02 22:40:43 +1200

    new styles and layout

--- src/App.js
+++ src/App.js
@@ -19,6 +19,31 @@ function App() {
           Learn React
         </a>
       </header>
+      <div id="droparea" />
+      <div id="container">
+        <div id="header">
+          <h1>
+            Sendsend
+          </h1>
+          <h2>
+            Simple peer to peer file transfer.
+          </h2>
+	</div>
+	<div id="status">
+	  <div id="status-inner">
+	    <span id="status-text" />
+          </div>
+        </div>
+        <div id="progress">
+          <div id="progress-first">
+          </div>
+          <div id="progress-second">
+          </div>
+        </div>
+        <div id="message">
+          <span id="message-text"></span>
+        </div>
+      </div>
     </div>
   );
 }
